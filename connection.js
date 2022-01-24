1decc5384bab88939255b474f61534451cd5658f
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-01-24 11:36:45 -0800

    make it better

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://dt187:dt187@cluster0.6uzwm.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    url: 'mongodb+srv://dt187:dt187@cluster0.6uzwm.mongodb.net/chat_app?retryWrites=true&w=majority',
     params: {
         useNewUrlParser: true,
         useUnifiedTopology: true
