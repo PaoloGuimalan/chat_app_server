095cbd65097a4267a556223d386cc943694ee81d
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2024-03-09 17:09:55 +0800

    feat: answer service had been added to socket

--- socketIO/socketIO.js
+++ socketIO/socketIO.js
@@ -81,6 +81,23 @@ const initSocketIO = (server) => {
             // console.log(callCollections[data.conversationID].users)
         })
 
+        socket.on("answer_data", (data) => {
+            var conversationID = data.conversationID;
+            var currentCall = callCollections[conversationID];
+
+            if(currentCall){
+                currentCall.users.filter((flt) => flt.socketID !== socketID).map((mp) => {
+                    // var usersInCallSocketMemory = callCollections[conversationID].users.map((mp) => mp.userID);
+                    const peerdata = data
+                    io.to(mp.socketID).emit('answer_peer_service', peerdata);
+                })
+            }
+
+            // console.log(socket.id, data);
+            // socket.to
+            // console.log(callCollections[data.conversationID].users)
+        })
+
         socket.on("leavecall", (data) => {
             // console.log("LEAVE CALL", data);
             var conversationID = data.conversationID;
