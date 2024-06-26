fcdeb3dbf6398ada2115e428152ef6ff95a8216e
Paolo <pauloportes.guimalan187@gmail.com>
2024-06-26 18:47:53 +0800

    fix: try catch around broker actions

--- reusables/rabbitmq/consumer.js
+++ reusables/rabbitmq/consumer.js
@@ -14,50 +14,54 @@ const POD_NAME = process.env.POD_NAME || "podless";
 
 function brokerActions (data) {
   const message = data.message;
-  const parameters = message.message.parameters;
+  try{
+    const parameters = message.message.parameters;
 
-  switch(data.event){
-    case SSE_NOTIFICATIONS_TRIGGER:
-      SSENotificationsTrigger(parameters.type, parameters.ids, parameters.details);
-      break;
-    case MESSAGES_TRIGGER_LOOPER:
-      parameters.receivers.map((rcvs) => {
-        MessagesTrigger(rcvs, parameters.sender, parameters.onseen);
-      });
-      break;
-    case CONTACT_LIST_TRIGGER_LOOPER:
-      parameters.receivers.map((rcvs) => {
-        ContactListTrigger(rcvs, parameters.details);
-      });
-      break;
-    case REACH_CALL_RECEPIENTS_LOOPER:
-      parameters.recepients.map((rcp) => {
-        ReachCallRecepients(rcp, parameters.decodedToken);
-      })
-      break;
-    case UPDATE_CONTATCS_W_SESSION_STATUS_LOOPER:
-      parameters.contacts.map((mp) => {
-        UpdateContactswSessionStatus(mp, parameters.decodedToken);
-      });
-      break;
-    case CALL_REJECT_NOTIF:
-      CallRejectNotif(parameters.rcp, parameters.decodedToken);
-      break;
-    case CALL_REJECT_NOTIF_LOOPER:
-      parameters.recepients.map((mp) => {
-        CallRejectNotif(mp, parameters.decodedToken);
-      })
-      break;
-    case SEND_TAG_POST_NOTIFICATION:
-      SendTagPostNotification(parameters.details, parameters.userID);
-      break;
-    case BROADCAST_IS_TYPING_STATUS_LOOPER:
-      parameters.receivers.map((mp) => {
-        BroadcastIsTypingStatus(mp, parameters.data);
-      })
-      break;
-    default:
-      break;
+    switch(data.event){
+      case SSE_NOTIFICATIONS_TRIGGER:
+        SSENotificationsTrigger(parameters.type, parameters.ids, parameters.details);
+        break;
+      case MESSAGES_TRIGGER_LOOPER:
+        parameters.receivers.map((rcvs) => {
+          MessagesTrigger(rcvs, parameters.sender, parameters.onseen);
+        });
+        break;
+      case CONTACT_LIST_TRIGGER_LOOPER:
+        parameters.receivers.map((rcvs) => {
+          ContactListTrigger(rcvs, parameters.details);
+        });
+        break;
+      case REACH_CALL_RECEPIENTS_LOOPER:
+        parameters.recepients.map((rcp) => {
+          ReachCallRecepients(rcp, parameters.decodedToken);
+        })
+        break;
+      case UPDATE_CONTATCS_W_SESSION_STATUS_LOOPER:
+        parameters.contacts.map((mp) => {
+          UpdateContactswSessionStatus(mp, parameters.decodedToken);
+        });
+        break;
+      case CALL_REJECT_NOTIF:
+        CallRejectNotif(parameters.rcp, parameters.decodedToken);
+        break;
+      case CALL_REJECT_NOTIF_LOOPER:
+        parameters.recepients.map((mp) => {
+          CallRejectNotif(mp, parameters.decodedToken);
+        })
+        break;
+      case SEND_TAG_POST_NOTIFICATION:
+        SendTagPostNotification(parameters.details, parameters.userID);
+        break;
+      case BROADCAST_IS_TYPING_STATUS_LOOPER:
+        parameters.receivers.map((mp) => {
+          BroadcastIsTypingStatus(mp, parameters.data);
+        })
+        break;
+      default:
+        break;
+    }
+  }catch(ex){
+    console.log("Action Invalid: ", data);
   }
 }
 
