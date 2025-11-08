70d9ad5a14d5f387b2073ed8c70f36660f76ebd7
Paolo <pauloportes.guimalan187@gmail.com>
2025-11-09 07:41:44 +0800

    fix: auto create preview count after creating new post

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -463,6 +463,14 @@ router.post("/createpost", jwtchecker, async (req, res) => {
         await client.query(insertTagQuery, tagValues);
       }
 
+      const insertPreviewCountsQuery = `
+        INSERT INTO newsfeed_previewcount (preview_id, post_id, emoji_id, count)
+        SELECT $1, $2, emoji_id, 0
+        FROM newsfeed_emoji
+      `;
+
+      await client.query(insertPreviewCountsQuery, [generateUUID(), postID]);
+
       await client.query("COMMIT");
 
       // Notify tagged users if any
