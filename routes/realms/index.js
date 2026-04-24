35ebdc3a997aa478fec02524c127e65cf0fc4659
Paolo <pauloportes.guimalan187@gmail.com>
2026-04-25 03:22:30 +0800

    feat: added remove user endpoint works for all realms

--- routes/realms/index.js
+++ routes/realms/index.js
@@ -6,6 +6,9 @@ const Storage = require("../../reusables/hooks/storage");
 const multiparty = require("multiparty");
 const fs = require("fs/promises");
 const makeid = require("../../reusables/hooks/makeID");
+const {
+  NotificationMessageForConversations,
+} = require("../../reusables/models/messages");
 const router = express.Router();
 
 router.post("/upload-media", jwtchecker, async (req, res) => {
@@ -92,6 +95,116 @@ router.post("/upload-media", jwtchecker, async (req, res) => {
       console.log(ex);
     }
   });
+});
+
+router.delete("/remove-user", jwtchecker, async (req, res) => {
+  const userID = req.params.userID;
+  const username = req.params.username;
+  const id = req.params.id;
+
+  try {
+    const account_ids = req.body.account_ids;
+    const realm_id = req.body.realm_id;
+
+    const { rows: users } = await pool.query(
+      `SELECT username FROM user_account WHERE id = ANY($1::text[])`,
+      [account_ids],
+    );
+
+    const { rows: row } = await pool.query(
+      `SELECT member_id FROM community_member WHERE account_id = $1 AND realm_id = $2 AND role = $3;`,
+      [id, realm_id, "admin"],
+    );
+
+    if (row.length === 0) {
+      const restids = account_ids.filter((flt) => flt !== id);
+
+      if (restids.length > 0) {
+        res.status(401).send({
+          status: false,
+          message: "You are not authorized to do this action",
+        });
+        return;
+      }
+    }
+
+    const { rows: realm_row } = await pool.query(
+      `SELECT * FROM community_realm WHERE realm_id = $1`,
+      [realm_id],
+    );
+
+    if (realm_row.length <= 0) {
+      res.status(400).send({
+        status: false,
+        message: "No realm found",
+      });
+      return;
+    }
+
+    const realm = realm_row[0];
+
+    if (realm.type === "server") {
+      const { rows: channels } = await pool.query(
+        `SELECT realm_id FROM community_realm WHERE parent_id = $1`,
+        [realm_id],
+      );
+
+      const channel_ids = channels.map((mp) => mp.realm_id);
+
+      const { rows: joined_channels } = await pool.query(
+        `SELECT realm_id FROM community_member WHERE account_id = ANY($1::text[]) AND realm_id = ANY($2::text[])`,
+        [account_ids, channel_ids],
+      );
+
+      const joined_channel_ids = joined_channels.map((mp) => mp.realm_id);
+
+      if (joined_channel_ids.length > 0) {
+        await pool.query(
+          `DELETE FROM community_member WHERE account_id = ANY($1::text[]) AND realm_id = ANY($2::text[])`,
+          [account_ids, joined_channel_ids],
+        );
+
+        users.map((mp) => {
+          joined_channel_ids.map((mpp) => {
+            NotificationMessageForConversations(
+              mpp,
+              userID,
+              [],
+              `${username} removed ${mp.username}`,
+              "server",
+            );
+          });
+        });
+      }
+    }
+
+    await pool.query(
+      `DELETE FROM community_member WHERE account_id = ANY($1::text[]) AND realm_id = $2`,
+      [account_ids, realm_id],
+    );
+
+    if (realm.type !== "page" && realm.type !== "server") {
+      users.map((mp) => {
+        NotificationMessageForConversations(
+          realm_id,
+          userID,
+          [],
+          `${username} removed ${mp.username}`,
+          realm.parent_id ? "server" : realm.type,
+        );
+      });
+    }
+
+    res.send({
+      status: true,
+      message: "OK",
+    });
+  } catch (ex) {
+    res
+      .status(500)
+      .send({ status: false, message: ex.message || ex.toString() });
+    console.log(ex);
+  }
 });
 
 module.exports = router;
